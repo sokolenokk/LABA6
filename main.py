414ff1cd4a444c4ba4e1224f9decea4937a280d3
sokolenok <skls77@mail.ru>
2025-12-13 01:02:48 +0300

    Add task 3: Name - Hello form

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 def home():
-    return {"message": "Hello, World!"}
+    return JSONResponse(
+        content={"message": "Hello, World!"},
+        media_type="application/json; charset=utf-8"
+    )
 
 
 @app.get("/greet/{name}")
 def greet(name: str):
-    return {"message": f"Привет, {name}!"}
+    return JSONResponse(
+        content={"message": f"Привет, {name}!"},
+        media_type="application/json; charset=utf-8"
+    )
+
+
+@app.get("/form")
+def show_form(request: Request):
+    return templates.TemplateResponse(
+        name="form.html",
+        context={"request": request}
+    )
+
+
+@app.post("/form")
+def process_form(request: Request, username: str = Form(...)):
+    greeting = f"Привет, {username}!"
+    return templates.TemplateResponse(
+        name="form.html",
+        context={"request": request, "greeting": greeting}
+    )
